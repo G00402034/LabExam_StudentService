ef695f98a666364c207dbce382956945eaf9272e
G00402034 <G00402034@atu.ie>
2024-10-21 12:08:05 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/lab_exam_ms1/LabExamMs1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam_ms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamMs1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamMs1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab_exam_ms1/LabExamMs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam_ms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamMs1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
